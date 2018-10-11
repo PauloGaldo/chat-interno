03ae6afc63466982e7fd4d7c3af2559a0ad51d5c
Paulo Galdo <paulo.galdo@incluit.com>
2018-10-11 09:51:49 -0300

    Eliminar contacto de grupo en creacion

--- src/app/chat/chat-contacts/chat-contacts.component.ts
+++ src/app/chat/chat-contacts/chat-contacts.component.ts
@@ -1,7 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { ChatService } from '../services/chat.service';
-import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
     selector: 'ci-chat-contacts',
@@ -19,75 +18,70 @@ export class ChatContactsComponent implements OnInit {
     public contactSelected = false;
     public contactDelete = false;
 
-        constructor(
-            private activatedRoute: ActivatedRoute,
-            private router: Router,
-            private chatService: ChatService
-        ) {
-    // CARGAR MOCK DE CONTACTOS
-    this.chatService.getContactList().subscribe(response => {
-        this.contacts = response.data;
-    });
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private router: Router,
+        private chatService: ChatService
+    ) {
+        // CARGAR MOCK DE CONTACTOS
+        this.chatService.getContactList().subscribe(response => {
+            this.contacts = response.data;
+        });
 
-    // CARGAR MOCK DE CHATS
-    this.chatService.getMessagesList().subscribe(response => {
-        this.chats = response.data;
-    });
-}
-
-ngOnInit() {
-}
-
-/**
- * Funcion para determinar que contacto esta activo del listado
- * @param contact contacto del listado
- */
-isContactActive(contact: any): boolean {
-    const queryParams: Params = Object.assign({}, this.activatedRoute.snapshot.queryParams);
-    if (contact.idPerfil === queryParams['id']) {
-        this.activeContact = contact;
-        return true;
+        // CARGAR MOCK DE CHATS
+        this.chatService.getMessagesList().subscribe(response => {
+            this.chats = response.data;
+        });
     }
-    return false;
-}
 
-/**
- * Metodo para seleccionar y poner activo un contacto del listado
- * @param contact contacto del listado
- */
-selectContact(contact: any): void {
-    if (contact) {
-        this.activeContact = contact;
+    ngOnInit() {
+    }
+
+    /**
+     * Funcion para determinar que contacto esta activo del listado
+     * @param contact contacto del listado
+     */
+    isContactActive(contact: any): boolean {
         const queryParams: Params = Object.assign({}, this.activatedRoute.snapshot.queryParams);
-        queryParams['id'] = contact.idPerfil;
-        this.router.navigate(['.'], { queryParams: queryParams });
-        this.changed.emit(contact);
+        if (contact.idPerfil === queryParams['id']) {
+            this.activeContact = contact;
+            return true;
+        }
+        return false;
     }
-}
 
-// Crear grupo
-
-createGroup() {
-this.optionGroup = true;
-}
-
-// Agregar contacto al grupo
-
-addContactGroup(contact: any) {
-this.contactSelected = true;
-this.contactsGroup.push(contact);
-}
-
-// Eliminar contacto del Grupo
-
-deleteContactGroup(contact: any) {
-    for (let c of this.contactsGroup) {
-        if (contact === c ) {
-            c = null;
-            console.log('contacto borrado');
-            console.log(this.contactsGroup);
-            this.contactDelete = true;
+    /**
+     * Metodo para seleccionar y poner activo un contacto del listado
+     * @param contact contacto del listado
+     */
+    selectContact(contact: any): void {
+        if (contact) {
+            this.activeContact = contact;
+            const queryParams: Params = Object.assign({}, this.activatedRoute.snapshot.queryParams);
+            queryParams['id'] = contact.idPerfil;
+            this.router.navigate(['.'], { queryParams: queryParams });
+            this.changed.emit(contact);
         }
     }
-  }
+
+    // Crear grupo
+    createGroup() {
+        this.optionGroup = true;
+    }
+
+    // Agregar contacto al grupo
+    addContactGroup(contact: any) {
+        this.contactSelected = true;
+        this.contactsGroup.push(contact);
+    }
+
+    /**
+     * Eliminar de seleccion de grupos a un contacto en particular usando
+     * su indice
+     * @param index indice de contacto
+     */
+    deleteContactGroup(index: any) {
+        this.contactsGroup.splice(index, 1);
+    }
+
 }
